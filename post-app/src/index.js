42f8241ba883f3096d8ad88aeb613443a75fed5b
kaihuan Huang <huanhuan@kaihuans-MacBook-Pro.local>
2023-01-10 22:07:00 -0800

    code review, remove not necessary code

--- post-app/src/index.js
+++ post-app/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { createRoot } from "react-dom";
 import './index.css';
 // import App from './RealEstateApp';
-import reportWebVitals from './reportWebVitals';
+
 // import 'antd/dist/antd.css';
 import RealEstateApp from './RealEstateApp';
 
@@ -20,4 +20,4 @@ createRoot.render(<RealEstateApp />, document.getElementById("root"))
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
