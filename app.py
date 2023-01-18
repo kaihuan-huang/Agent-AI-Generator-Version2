8c42b700120732b1c5aaac7ce49f22739ae7f691
kaihuan Huang <huanhuan@kaihuans-MacBook-Pro.local>
2023-01-17 22:12:09 -0800

    testing is connecting to openai api and get the feedback

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import requests
 from flask import Flask,render_template, request, jsonify
 import openai
+import os
+
 
 # Initialize the Flask app
 app = Flask(__name__)
@@ -16,8 +18,6 @@ def fetch_property_data():
     if request.method == "POST":
         print(request.form['code'])
         
-    
-    
     # Get the listing ID from the request
     listing_id = request.args.get("listing_id")
     # listing_id = request.form['address']
@@ -47,16 +47,57 @@ def fetch_property_data():
     
     
 
+# # Define the endpoint for generating post content using GPT-3
+# @app.route("/generate/", methods=["GET","POST"])
+# def generate_content():
+#     # Get the input from the request
+#     if request.method == "POST":
+#         print(request.form['input_text'])
+        
+#     input_text = request.json["input_text"]
+#     print('input_text',input_text)
+
+#     # Use openai_secret_manager to fetch the API key
+#     import openai_secret_manager
+#     secrets = openai_secret_manager.get_secrets("openai")
+#     openai.api_key = secrets["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"]
+
+#     # Use GPT-3 to generate post content
+#     response = openai.Completion.create(
+#         engine="text-davinci-002",
+#         prompt=f"{input_text}",
+#         temperature=0.8,
+#     )
+#     post_content = response["choices"][0]["text"]
+
+#     # Return the generated content
+#     return jsonify(post_content)
+
+# # if __name__ == "__main__":
+# #     app.run(debug=True, port='9000')
+# if __name__ == '__main__':
+#     app.run(host='0.0.0.0', port='8888', debug=True)
+
 # Define the endpoint for generating post content using GPT-3
-@app.route("/generate/", methods=["POST"])
+@app.route("/generate/", methods=["GET","POST"])
 def generate_content():
     # Get the input from the request
-    input_text = request.json["input_text"]
+    if request.method == "POST":
+        print(request.form) # Debugging line
+        input_text = request.form['input_text']
+    else:
+        print(request.json) # Debugging line
+        input_text = request.json["input_text"]
+    print('input_text',input_text)
+    
+       # Use an environment variable to set the API key
+    openai.api_key = "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"
+
 
     # Use openai_secret_manager to fetch the API key
-    import openai_secret_manager
-    secrets = openai_secret_manager.get_secrets("openai")
-    openai.api_key = secrets["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"]
+    # import openai_secret_manager
+    # secrets = openai_secret_manager.get_secrets("openai")
+    # openai.api_key = secrets["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"]
 
     # Use GPT-3 to generate post content
     response = openai.Completion.create(
@@ -74,6 +115,5 @@ def generate_content():
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='8888', debug=True)
 
-    
 ## have defined two endpoints in the Flask app, one for fetching data from the RESO API and another one for generating post content using GPT-3. The first endpoint is a GET request to the "/property" endpoint, which is using the listing ID passed as a query parameter to fetch data from the RESO API, it then returns the data in a JSON format.
 ## The second endpoint is a POST request to the "/generate" endpoint, which takes the input from the user and uses GPT-3 to generate post content, the post content is returned in a JSON format.
